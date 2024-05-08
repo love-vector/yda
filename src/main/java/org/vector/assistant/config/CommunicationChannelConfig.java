0ebede282b064568a07e23e42fb6fbd0c0f5e75a
Ruslan Kliushenkov <r.kliushenkov@vector-inc.dev>
2024-05-08 13:30:44 +0300

    [YDA-30] basic code update

--- src/main/java/org/vector/assistant/config/CommunicationChannelConfig.java
+++ src/main/java/org/vector/assistant/config/CommunicationChannelConfig.java
@@ -3,4 +3,4 @@ package org.vector.assistant.config;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class ResourceCommunicationChannelConfig {}
+public class CommunicationChannelConfig {}
