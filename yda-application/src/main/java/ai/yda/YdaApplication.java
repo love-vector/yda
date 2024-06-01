337a25cb10d60cdb325dbd4eab82acb2df96b7e8
Nikita Litvinov <n.litvinov@vector-inc.dev>
2024-06-01 12:32:08 +0300

    small change to application to get llmProvider

--- yda-application/src/main/java/ai/yda/YdaApplication.java
+++ yda-application/src/main/java/ai/yda/YdaApplication.java
@@ -1,12 +1,21 @@
 package ai.yda;
 
+import lombok.RequiredArgsConstructor;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+import ai.yda.framework.llm.LlmProvider;
 
 @SpringBootApplication
+@RequiredArgsConstructor
 public class YdaApplication {
 
+    private final ApplicationContext applicationContext;
+
     public static void main(String[] args) {
-        SpringApplication.run(YdaApplication.class, args);
+        ApplicationContext context = SpringApplication.run(YdaApplication.class, args);
+        LlmProvider llmProvider = context.getBean(LlmProvider.class);
     }
 }
