edc9a8a510f5b0d8a5ef9ae550d46391a7e7bb51
dmitry <dmmrch@gmail.com>
2024-06-18 10:03:45 +0200

    reworked Assistant into azure-sdk-beta3

--- rag/generators/openai-assistant-generator/src/main/java/ai/yda/framework/generator/assistant/openai/OpenAiAssistantGenerator.java
+++ rag/generators/openai-assistant-generator/src/main/java/ai/yda/framework/generator/assistant/openai/OpenAiAssistantGenerator.java
@@ -1,9 +1,6 @@
 package ai.yda.framework.generator.assistant.openai;
 
 import java.util.List;
-import java.util.concurrent.Executors;
-import java.util.concurrent.TimeUnit;
-import java.util.concurrent.atomic.AtomicReference;
 
 import com.azure.ai.openai.assistants.AssistantsClient;
 import com.azure.ai.openai.assistants.models.*;
@@ -26,59 +23,40 @@ public class OpenAiAssistantGenerator implements Generator<BaseAssistantRequest,
 
     @Override
     public BaseAssistantResponse generate(BaseAssistantRequest request, RagContext context) {
-        //        var threadRun = addMessageAndRun(request.getContent(),
-        // getSessionProvider().getSession().getThreadId();
-        var threadRun = addMessageAndRun(request.getContent(), null);
-        var runResult = waitForResult(threadRun);
-        return processResponse(runResult);
-    }
 
-    private ThreadRun addMessageAndRun(String message, String threadId) {
-        if (threadId == null) {
-            return assistantsClient.createThreadAndRun(new CreateAndRunThreadOptions(assistantId)
-                    .setThread(new AssistantThreadCreationOptions()
-                            .setMessages(List.of(new ThreadInitializationMessage(MessageRole.USER, message)))));
+        var createAndRunThreadOptions = new CreateAndRunThreadOptions(assistantId)
+                .setThread(new AssistantThreadCreationOptions()
+                        .setMessages(List.of(new ThreadMessageOptions(MessageRole.USER, request.getContent()))));
+        var run = assistantsClient.createThreadAndRun(createAndRunThreadOptions);
+
+        do {
+            run = assistantsClient.getRun(run.getThreadId(), run.getId());
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        } while (run.getStatus() == RunStatus.QUEUED || run.getStatus() == RunStatus.IN_PROGRESS);
+
+        PageableList<ThreadMessage> messages = assistantsClient.listMessages(run.getThreadId());
+        List<ThreadMessage> data = messages.getData();
+        BaseAssistantResponse response = null;
+
+        for (int i = 0; i < data.size(); i++) {
+            ThreadMessage dataMessage = data.get(i);
+            MessageRole role = dataMessage.getRole();
+            if (role.equals(MessageRole.ASSISTANT)) {
+                for (MessageContent messageContent : dataMessage.getContent()) {
+                    MessageTextContent messageTextContent = (MessageTextContent) messageContent;
+                    System.out.println(i + ": Role = " + role + ", content = "
+                            + messageTextContent.getText().getValue());
+                    response = BaseAssistantResponse.builder()
+                            .content(messageTextContent.getText().getValue())
+                            .build();
+                }
+            }
         }
-        assistantsClient.createMessage(threadId, MessageRole.USER, message);
-        return assistantsClient.createRun(threadId, new CreateRunOptions(assistantId));
-    }
-
-    private ThreadRun waitForResult(ThreadRun threadRun) {
-        var atomicRun = new AtomicReference<ThreadRun>();
-        try (var scheduler = Executors.newScheduledThreadPool(1)) {
-            scheduler.schedule(
-                    () -> {
-                        var run = assistantsClient.getRun(threadRun.getThreadId(), threadRun.getId());
-                        if (!run.getStatus().equals(RunStatus.IN_PROGRESS)) {
-                            atomicRun.set(run);
-                            scheduler.shutdown();
-                        }
-                    },
-                    1,
-                    TimeUnit.SECONDS);
-            while (!scheduler.awaitTermination(500, TimeUnit.MILLISECONDS)) {}
-            return atomicRun.get();
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    private String getRunResult(ThreadRun threadRun) {
-        var threadMessage = assistantsClient
-                .listMessages(threadRun.getThreadId(), 1, ListSortOrder.DESCENDING, null, null)
-                .getData()
-                .getFirst();
-        return ((MessageTextContent) threadMessage.getContent().getFirst())
-                .getText()
-                .getValue();
-    }
-
-    private BaseAssistantResponse processResponse(ThreadRun threadRun) {
-        if (threadRun.getStatus().equals(RunStatus.FAILED)) {
-            throw new RuntimeException(threadRun.getLastError().getMessage());
-        }
-        var result = getRunResult(threadRun);
-        return BaseAssistantResponse.builder().content(result).build();
+        return response;
     }
 
     @Override
