8ef19145c0beced15f1ff442aa6d484d4393c6e3
Bogdan <b.synenko@vector-inc.dev>
2024-06-19 07:49:44 +0300

    YDAL-62 first crowler implementation

--- rag/foundation/base/src/main/java/ai/yda/framework/rag/base/retriever/WebSiteRetriever.java
+++ rag/foundation/base/src/main/java/ai/yda/framework/rag/base/retriever/WebSiteRetriever.java
@@ -1,7 +1,7 @@
 package ai.yda.framework.rag.base.retriever;
 
 import ai.yda.common.shared.model.impl.BaseAssistantRequest;
-import ai.yda.framework.rag.core.model.impl.BaseRagContext;
+import ai.yda.framework.rag.base.model.BaseRagContext;
 import ai.yda.framework.rag.core.retriever.Retriever;
 
 public class WebSiteRetriever implements Retriever<BaseAssistantRequest, BaseRagContext> {
