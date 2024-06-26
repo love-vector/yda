8c4fe40a4495aff4759bb93c139037960c3bef35
Bogdan <b.synenko@vector-inc.dev>
2024-06-26 13:35:04 +0300

    YDAL-62 Update configure settup

--- rag/autoconfigure/spring/retrievers/yda-retriever-filesystem-autoconfigure/src/main/java/ai/yda/framework/rag/retriever/website/autoconfigure/RetrieverFilesystemAutoConfiguration.java
+++ rag/autoconfigure/spring/retrievers/yda-retriever-filesystem-autoconfigure/src/main/java/ai/yda/framework/rag/retriever/website/autoconfigure/RetrieverFilesystemAutoConfiguration.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package ai.yda.framework.rag.retriever.filesystem.autoconfigure;
+package ai.yda.framework.rag.retriever.website.autoconfigure;
 
 import java.util.HashMap;
 
