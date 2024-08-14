e7c4e3c619b54ffbce973e499e31e3594a1e8762
Iryna kopchak <i.kopchak@vector-inc.dev>
2024-08-14 15:57:08 +0300

    YDAL-80 Add GNU Lesser General Public License

--- rag/core/src/main/java/ai/yda/framework/rag/core/util/ContentUtil.java
+++ rag/core/src/main/java/ai/yda/framework/rag/core/util/ContentUtil.java
@@ -1,3 +1,22 @@
+/*
+ * YDA - Open-Source Java AI Assistant
+ * Copyright (C) 2024 Love Vector OÜ <https://vector-inc.dev/>
+
+ * This file is part of YDA.
+
+ * YDA is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+
+ * YDA is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with YDA.  If not, see <https://www.gnu.org/licenses/>.
+*/
 package ai.yda.framework.rag.core.util;
 
 import java.nio.charset.StandardCharsets;
