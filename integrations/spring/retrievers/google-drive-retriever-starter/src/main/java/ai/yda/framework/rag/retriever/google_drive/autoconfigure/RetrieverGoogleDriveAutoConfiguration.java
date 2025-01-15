3246c36f9fa46beeff0593811b43b2338921ad7a
BogdanSynenko <163296081+BogdanSynenko@users.noreply.github.com>
2025-01-15 02:29:24 -0600

    Ydal 135 ai summary improve (#89)
    * YDAL-135 update logic

--- integrations/spring/retrievers/google-drive-retriever-starter/src/main/java/ai/yda/framework/rag/retriever/google_drive/autoconfigure/RetrieverGoogleDriveAutoConfiguration.java
+++ integrations/spring/retrievers/google-drive-retriever-starter/src/main/java/ai/yda/framework/rag/retriever/google_drive/autoconfigure/RetrieverGoogleDriveAutoConfiguration.java
@@ -21,6 +21,7 @@ package ai.yda.framework.rag.retriever.google_drive.autoconfigure;
 
 import java.io.IOException;
 import java.security.GeneralSecurityException;
+import java.time.Duration;
 
 import com.zaxxer.hikari.HikariDataSource;
 
@@ -28,17 +29,21 @@ import org.springframework.ai.autoconfigure.openai.OpenAiConnectionProperties;
 import org.springframework.ai.autoconfigure.openai.OpenAiEmbeddingProperties;
 import org.springframework.ai.autoconfigure.vectorstore.milvus.MilvusServiceClientProperties;
 import org.springframework.ai.autoconfigure.vectorstore.milvus.MilvusVectorStoreProperties;
-import org.springframework.ai.chat.model.ChatModel;
 import org.springframework.ai.openai.OpenAiChatModel;
+import org.springframework.ai.openai.api.OpenAiApi;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.boot.web.client.ClientHttpRequestFactories;
+import org.springframework.boot.web.client.ClientHttpRequestFactorySettings;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.core.io.ResourceLoader;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.web.client.RestClient;
+import org.springframework.web.reactive.function.client.WebClient;
 
 import ai.yda.framework.rag.retriever.google_drive.GoogleDriveRetriever;
 import ai.yda.framework.rag.retriever.google_drive.adapter.DocumentMetadataAdapter;
@@ -134,8 +139,20 @@ public class RetrieverGoogleDriveAutoConfiguration {
     }
 
     @Bean
-    public DocumentSummaryService documentSummaryService(final ChatModel chatModel) {
-        return new DocumentSummaryService(chatModel);
+    public DocumentSummaryService documentSummaryService(
+            final OpenAiConnectionProperties openAiConnectionProperties, final WebClient.Builder webClientBuilder) {
+        var restClientBuilder = RestClient.builder()
+                .requestFactory(ClientHttpRequestFactories.get(ClientHttpRequestFactorySettings.DEFAULTS
+                        .withConnectTimeout(Duration.ofMinutes(5))
+                        .withReadTimeout(Duration.ofMinutes(10))));
+        // TODO Come up with another way to modify OpenAiChatModel
+        var openAiApi = new OpenAiApi(
+                openAiConnectionProperties.getBaseUrl(),
+                openAiConnectionProperties.getApiKey(),
+                restClientBuilder,
+                webClientBuilder);
+
+        return new DocumentSummaryService(new OpenAiChatModel(openAiApi));
     }
 
     @Bean
@@ -149,7 +166,7 @@ public class RetrieverGoogleDriveAutoConfiguration {
             final MilvusServiceClientProperties milvusClientProperties,
             final OpenAiConnectionProperties openAiConnectionProperties,
             final OpenAiEmbeddingProperties openAiEmbeddingProperties,
-            final OpenAiChatModel openAiChatModel)
+            final WebClient.Builder webClientBuilder)
             throws IOException, GeneralSecurityException {
 
         var resource = resourceLoader.getResource(googleDriveProperties.getServiceAccountKeyFilePath());
@@ -175,6 +192,6 @@ public class RetrieverGoogleDriveAutoConfiguration {
                         documentMetadataPort,
                         documentProcessorProvider,
                         documentMetadataMapper,
-                        documentSummaryService(openAiChatModel)));
+                        documentSummaryService(openAiConnectionProperties, webClientBuilder)));
     }
 }
--- rag/retrievers/google-drive-retriever/src/main/java/ai/yda/framework/rag/retriever/google_drive/service/DocumentSummaryService.java
+++ rag/retrievers/google-drive-retriever/src/main/java/ai/yda/framework/rag/retriever/google_drive/service/DocumentSummaryService.java
@@ -16,18 +16,21 @@
 
  * You should have received a copy of the GNU Lesser General Public License
  * along with YDA.  If not, see <https://www.gnu.org/licenses/>.
- */
+*/
 package ai.yda.framework.rag.retriever.google_drive.service;
-
-import ai.yda.framework.rag.retriever.google_drive.entity.DocumentContentEntity;
-import ai.yda.framework.rag.retriever.google_drive.entity.DocumentMetadataEntity;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.ai.chat.model.ChatModel;
-import org.springframework.ai.document.Document;
-import org.springframework.ai.transformer.SummaryMetadataEnricher;
 
 import java.util.List;
 import java.util.stream.Collectors;
+
+import lombok.extern.slf4j.Slf4j;
+
+import org.springframework.ai.chat.model.ChatModel;
+import org.springframework.ai.document.Document;
+import org.springframework.ai.document.MetadataMode;
+import org.springframework.ai.transformer.SummaryMetadataEnricher;
+
+import ai.yda.framework.rag.retriever.google_drive.entity.DocumentContentEntity;
+import ai.yda.framework.rag.retriever.google_drive.entity.DocumentMetadataEntity;
 
 @Slf4j
 public class DocumentSummaryService {
@@ -39,9 +42,26 @@ public class DocumentSummaryService {
     }
 
     public String summarizeDocument(final DocumentMetadataEntity metadataDocuments) {
+        var documentSummaryInstruction = """
+        Provide a summary of the attached document by highlighting its key points.
+        Focus on the main arguments, supporting evidence, and conclusions drawn within the document.
+        The summary should be concise yet comprehensive, capturing the essence of the document's content.
+        For each document, ensure the following details are included at the beginning of the summary:
+        - File Name: Include the file name or mention 'File name is missing' if unavailable.
+        - Document Description: Include the document description or mention 'Description is missing' if unavailable.
+
+        Document Content:
+        {context_str}
+
+        Output Format:
+        - Present the summary in paragraph form, using bullet points if necessary to delineate distinct ideas or sections.
+        """;
         var transformDocuments = transformDocument(metadataDocuments);
-        var documentsEnricher =
-                new SummaryMetadataEnricher(chatModel, List.of(SummaryMetadataEnricher.SummaryType.CURRENT));
+        var documentsEnricher = new SummaryMetadataEnricher(
+                chatModel,
+                List.of(SummaryMetadataEnricher.SummaryType.CURRENT),
+                documentSummaryInstruction,
+                MetadataMode.ALL);
         var documentSummary = documentsEnricher.apply(List.of(transformDocuments));
         return extractSummary(documentSummary.get(0));
     }
@@ -51,7 +71,9 @@ public class DocumentSummaryService {
                 .map(DocumentContentEntity::getChunkContent)
                 .collect(Collectors.joining(""));
         return Document.builder()
-                .text(documentContent + metadataDocument.getDescription() + metadataDocument.getName())
+                .text("file name: " + metadataDocument.getName()
+                                + "\n document description: " + metadataDocument.getDescription()
+                                + "\ndocument content: " + documentContent)
                 .id(metadataDocument.getDocumentId())
                 .build();
     }
